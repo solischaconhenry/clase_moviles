cdbb14c9b154b32268ba625790bf0b0e496eb7f2
hsolicha <hsolicha@hotmail.com>
2016-03-02 15:15:23 -0600

    Clase 4

---
+++ shopAdvisor/app/src/main/java/cr/ac/itcr/shopadvisor/LoginActivity.java
@@ -0,0 +1,28 @@
+package cr.ac.itcr.shopadvisor;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLogin.setText("Successful");
+            }
+        });
+    }
+
+}
